401c891e643c84cdb90d491248c101832bf3bf74
neo.lee <w0435748@nscc.ca>
2020-11-26 11:26:29 -0400

    Apply heroku-postbuild

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));  //First looking for the default url
+app.use(express.static(path.join(__dirname, 'client/build')));  //First looking for the default url
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
